b8c5d25d0d83a2560f48802bb3118e46d264c0f6
MUHAMMAD THALHA <144235908+MUHAMMAD-THALHA@users.noreply.github.com>
2023-11-21 12:42:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+function Vi(){
+    var pop=document.getElementById("Virender")
+    pop.classList.toggle("Virender");
+  }
+  function Sa(){
+    var pop=document.getElementById("")
+    pop.classList.toggle("show");
+  }
+  function G(){
+    var pop=document.getElementById("pic3")
+    pop.classList.toggle("show");
+  }
+  function V(){
+    var pop=document.getElementById("pic4")
+    pop.classList.toggle("show");
+  }
+  function MS(){
+    var pop=document.getElementById("pic5")
+    pop.classList.toggle("show");
+  }
+  function Yuvaraj(){
+    var pop=document.getElementById("pic6")
+    pop.classList.toggle("show");
+  }
+  function S(){
+    var pop=document.getElementById("Suresh")
+    pop.classList.toggle("show");
+  }
+  function S(){
+    var pop=document.getElementById("pic8")
+    pop.classList.toggle("show");
+  }
+  function S(){
+    var pop=document.getElementById("pic9")
+    pop.classList.toggle("show");
+  }function S(){
+    var pop=document.getElementById("pic10")
+    pop.classList.toggle("show");
+  }
+  function T(){
+    var pop=document.getElementById("pic11")
+    pop.classList.toggle("show");
+  }
